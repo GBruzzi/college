4557f2f4878b7cf48a54f00117af3bdac5a559b6
GBruzzi <gbruzzi@sga.pucminas.br>
2024-05-27 22:01:30 -0300

    vetor dinamico

---
+++ 1 period/aeds1/p2/vetorDinamico.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+  int tam;
+  printf("digite o tamanho do vetor ");
+  scanf("%i", &tam);
+
+  int *vet = (int*)malloc(tam * sizeof(int));
+
+  if (vet == NULL)
+    return -1;
+
+  for (int i = 0; i < tam; i ++) {
+    vet[i] = i * 5;
+  }
+
+  for (int i = 0; i < tam; i ++) {
+    printf("%i ", *(vet + i));
+  }
+
+
+  return 0;
+}
